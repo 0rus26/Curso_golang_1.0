a0607c667e489f6fcec8b2cc91444f512396a872
0rus26 <jrsalas26@gmail.com>
2024-04-21 12:11:53 -0500

    ejercicios 6 y 7, termiandos vamos siuu

---
+++ 7nv_Ejercicios/2_ejercicio.go/ejercicio2.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+/*
+
+●	Crea un strutc persona
+●	Crea una función llamada “cambiame” con un *persona como parámetro
+○	Cambia el valor almacenado en la dirección de memoria del *persona
+■	Importante
+●	Para desreferenciar un struct, usa (*valor).campo
+○	p1.nombre
+○	(*p1).nombre
+●	“Como una excepción, si el tipo de x es un tipo puntero con nombre y (*x).c es una expresión válida de selección denotando un campo (pero no un método), x.c es una forma abreviada de (*x).c”.
+○	https://golang.org/ref/spec#Selectors
+●	Crea un valor de tipo persona
+○	Imprime el valor
+●	En func main
+○	llama “cambiame”
+●	En func main
+○	Imprime el valor
+
+
+*/
+
+//este ejercicio sirve para entender que go otorga un espacio en memoria diferente para hacer el cambio de p.nombre, por lo tanto si queremos que p1
+//sea afectadi por la función cambiame debemos apuntar a la dirección en memoria de ese  dato persona.
+func main() {
+
+	p1 := persona{
+		id:     111,
+		nombre: "Superman",
+	}
+
+	fmt.Println(p1.nombre)
+	cambiame(&p1)
+	fmt.Println(p1.nombre)
+}
+
+type persona struct {
+	id     int
+	nombre string
+}
+
+func cambiame(p *persona) {
+	p.nombre = "Spiderman"
+}
---
+++ 7nv_Ejercicios/notas_7.go
@@ -0,0 +1,48 @@
+package main
+
+/*
+
+utiles para pasar la direccion de una variable que sea bastante grande PUNTEROS
+utiles para modificar en memoria el valor de una variable,
+func main() {
+
+	x := 42
+	fmt.Println(&x)
+	fmt.Printf("%T,%v\n", x, x)
+	fmt.Printf("%T,%v\n", &x, &x)
+
+	b := &x
+	fmt.Println(b)
+	fmt.Println(*b)
+}
+
+*/
+
+/*
+
+pointers
+
+&var es para acceder al valor
+*var para acceder al espacio en memoria
+
+func foo(y *int) {
+	fmt.Println("Y antes", y)
+	fmt.Println("Y antes**", *y)
+	*y = 42
+	fmt.Println("Y despues", y)
+	fmt.Println("Y despues**", *y)
+}
+
+func main() {
+	x := 0
+	fmt.Println("X antes", x)
+	fmt.Println("X antes**", &x)
+	foo(&x)
+	fmt.Println("X despues", x)
+	fmt.Println("X despues**", &x)
+
+}
+*/
+func main() {
+
+}
